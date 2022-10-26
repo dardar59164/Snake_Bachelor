c34d5f1d5229e1c548362ba09517200ee7e873ce
Thomas_D <t.dare@ludus-academie.com>
2022-10-26 11:59:27 +0200

    Board

---
+++ Snake_Bachelor/Game/Board.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Bachelor.Game
+{
+    public class Board
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Board()
+        {
+            Width = 100;
+            Height = 100;
+        }
+
+        public void DrawBoard()
+        {
+            Console.Clear();
+            for (int i = 0; i < Width; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write('#');
+            }
+            for (int i = 0; i < Width; i++)
+            {
+                Console.SetCursorPosition(i, Height);
+                Console.Write('#');
+            }
+            for (int i = 0; i < Height; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write('#');
+            }
+            for (int i = 0; i < Height; i++)
+            {
+                Console.SetCursorPosition(Width, i);
+                Console.Write('#');
+            }
+        }
+    }
+}
