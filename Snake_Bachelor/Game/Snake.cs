15618ae8b7b8f8df23a527e07b29453cc0dbed93
Thomas_D <t.dare@ludus-academie.com>
2022-10-26 13:36:34 +0200

    Snake Base

---
+++ Snake_Bachelor/Game/Snake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Bachelor.Game
+{
+    public class Snake
+    {
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+
+        public int y { get; set; }
+        public int x { get; set; }
+
+        public Snake()
+        {
+            x = 30;
+            y = 15;
+        }
+
+        public void drawSnake()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("O");
+        }
+
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                char key = keyInfo.KeyChar;
+            }
+        }
+    }
+}
